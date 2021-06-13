8c2ecc89fe4d11cdc654a673c4cd0449ff9458b0
Ju1ija <j.lipnickaja@gmail.com>
2021-06-13 17:45:51 +0100

    added API call

--- local-weather-app.js
+++ local-weather-app.js
@@ -1,3 +1,22 @@
 (function () {
-    
+    function success(pos) {
+        let crd = pos.coords;
+
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+        getLocalWeather(crd.latitude, crd.longitude);
+    }
+
+    navigator.geolocation.getCurrentPosition(success);
+
+    function getLocalWeather(lat, lon) {
+        $.get(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${lat}&lon=${lon}`, handleWeatherData);
+    }
+
+    function handleWeatherData(data) {
+        console.log(data);
+        $("#location").text(data.name);
+    }
 })();
