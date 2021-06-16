a2f0b8572c8d14dc6dcb6732e06e0f68b2ba6622
Ju1ija <j.lipnickaja@gmail.com>
2021-06-16 20:50:42 +0100

    Added weather icons & changed button location

--- local-weather-app.js
+++ local-weather-app.js
@@ -1,4 +1,11 @@
 (function () {
+    let temp = 0;
+
+    $(document).ready(function() {
+        $("#fahrenheit").click(changeCelsiusToFahrenheit);
+        $("#celsius").click(changeFahrenheitToCelsius);
+    });
+
     function success(pos) {
         let crd = pos.coords;
 
@@ -17,10 +24,40 @@
 
     function handleWeatherData(data) {
         console.log(data);
+        temp = Math.round(data.main.temp * 10) / 10;
         $("#location").text(data.name);
-        $("#temperature").text(Math.round(data.main.temp));
+        $("#temperature").text(temp + "\u00B0C");
         $("#description").text(data.weather[0].main);
-        $("#image").attr("src", data.weather[0].icon);
+        // $("#weather-icon").attr("src", data.weather[0].icon);
         $(".card").show("slow");
+        weatherIcon();
+
+        function weatherIcon() {
+            if(data.weather[0].main == "Drizzle") {
+                $("#weather-icon").attr("class", "bi bi-cloud-drizzle");
+            } else if(data.weather[0].main == "Thunderstorm") {
+                $("#weather-icon").attr("class", "bi bi-cloud-lightning");
+            } else if(data.weather[0].main == "Rainy") {
+                $("#weather-icon").attr("class", "bi bi-cloud-rain");
+            } else if(data.weather[0].main == "Snowy") {
+                $("#weather-icon").attr("class", "bi bi-cloud-snow");
+            } else if(data.weather[0].main == "Cloudy") {
+                $("#weather-icon").attr("class", "bi bi-clouds");
+            } else if(data.weather[0].main == "Clear") {
+                $("#weather-icon").attr("class", "bi-sun");
+            } else if(data.weather[0].main == "Windy") {
+                $("#weather-icon").attr("class", "bi bi-wind");
+            } else {
+                $("#weather-icon").attr("class", "bi bi-emoji-dizzy");
+            }
+        }
+    }
+
+    function changeCelsiusToFahrenheit() {
+        $("#temperature").text(Math.round(((temp * 9 / 5) + 32) * 10) / 10 + "\u00B0F");
+    }
+
+    function changeFahrenheitToCelsius() {
+        $("#temperature").text(temp + "\u00B0C");
     }
 })();
